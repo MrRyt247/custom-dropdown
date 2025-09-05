62aa3729da8de56e239b1594dca34964c12f5d9a
Mr. Ryt <flaviosobbin04@gmail.com>
2025-09-05 11:44:31 +0000

    :art: enhance dropdown component with new styles and functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
 	import { data } from './data/data';
+	import '@fontsource/comic-relief';
+	import { ChevronDown, Icon } from 'svelte-hero-icons';
 
 	let selected: string = $state(data[0].select);
 	let isOpened: boolean = $state(false);
+	let dropdownRef: HTMLDivElement;
+
+	function handleClickOutside(event: MouseEvent) {
+		if (dropdownRef && !dropdownRef.contains(event.target as Node)) {
+			isOpened = false;
+		}
+	}
+
+	$effect(() => {
+		if (isOpened) {
+			document.addEventListener('click', handleClickOutside);
+		} else {
+			document.removeEventListener('click', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	});
 </script>
 
-<div class="select">
+<h1 style="margin-bottom: 0.5rem;">Custom Dropdown</h1>
+
+<div class="select" bind:this={dropdownRef}>
 	<button type="button" class="select-container" onclick={() => (isOpened = !isOpened)}>
 		<span>{selected}</span>
-		<span style:rotate={isOpened ? '-180deg' : '0deg'}>&downarrow;</span>
+		<span style:rotate={isOpened ? '-180deg' : '0deg'}>
+			<Icon src={ChevronDown} size="1.25rem" />
+		</span>
 	</button>
 	<ul style:display={isOpened ? 'flex' : 'none'}>
 		{#each data as item}
@@ -36,11 +61,12 @@
 		box-sizing: border-box;
 		margin: 0;
 		padding: 0;
+		font-family: 'Comic Relief', sans-serif;
 	}
 
 	.select {
 		display: flex;
-		width: 15em;
+		width: 18em;
 		font-family: inherit;
 		font-size: inherit;
 		line-height: inherit;
@@ -52,6 +78,7 @@
 			border-radius: 10px;
 
 			span:last-child {
+				display: flex;
 				transition: rotate 300ms ease;
 			}
 		}
